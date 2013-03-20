6d5b1281c9dc8bf8998cf1d085c46753cce72164
Óscar Nájera <najera.oscar@gmail.com>
2013-03-20 22:46:10 +0100

    make test now compares results of OpenMP version of original code, OpenCL version in cpu & OpenMP-OpenCl version against original version results

--- test/src/tester.py
+++ test/src/tester.py
@@ -3,12 +3,20 @@ import numpy as np
 
 
 def main():
-  reference_data = np.genfromtxt('argon_108_base.dat')
-  test_data	 = np.genfromtxt('argon_108_CL.dat')
+  print "Parallel OpenMP original"
+  reference_data = np.genfromtxt('argon_108.dat.base')
+  test_data	 = np.genfromtxt('argon_108.dat.ljmd-ori.opti')
+  print abs(reference_data-test_data)[:,4]
   
+  print "OpenCL on cpu"
+  reference_data = np.genfromtxt('argon_108.dat.base')
+  test_data	 = np.genfromtxt('argon_108.dat.ljmd-cl')
   print abs(reference_data-test_data)[:,4]
 
-
+  print "OpenCL on cpu OpenMP"
+  reference_data = np.genfromtxt('argon_108.dat.base')
+  test_data	 = np.genfromtxt('argon_108.dat.ljmd-cl.opti')
+  print abs(reference_data-test_data)[:,4]
 
 if __name__ == "__main__":
     main()
