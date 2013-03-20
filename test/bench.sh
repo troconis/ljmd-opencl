95b39a0858975ba4dd0c0696086e41dd7749d6c2
Ajasja Ljubeti <ajasja.ljubetic@gmail.com>
2013-03-20 19:35:57 +0100

    Added benchmark scripts
    Added benchmarks scripts to time the dependance on the threads.

---
+++ test/bench.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#utility to bench  the time run vs the number of spawned threads
+
+device=$1
+threads=$2
+infile=$3
+benchfile=$4
+echo "device $device threads $threads infile $infile benchfile $benchfile"
+
+rm $benchfile
+for t in $threads 
+do
+    #echo "$t: device $device threads $threads infile $infile benchfile $benchfile"
+    /usr/bin/time  --output=$benchfile --append --format="%C %e seconds %K kilobytes" ./ljmd_CL cpu $t < $infile 
+done
+
